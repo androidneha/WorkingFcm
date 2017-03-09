0919bc2b74ecfd33d6cee8300fc63b5f4b227bbd
Neha <nehaappdev@gmail.com>
2017-03-09 10:30:55 +0530

    Formated Code

--- app/src/main/java/com/example/finalfcm/MainActivity.java
+++ app/src/main/java/com/example/finalfcm/MainActivity.java
@@ -4,54 +4,37 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
     private static final String TAG = "MainActivity";
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         checkPermissionsOfApp(getPackageName());
-        // If a notification message is tapped, any data accompanying the notification
-        // message is available in the intent extras. In this sample the launcher
-        // intent is fired when the notification is tapped, so any accompanying data would
-        // be handled here. If you want a different intent fired, set the click_action
-        // field of the notification message to the desired intent. The launcher intent
-        // is used when no click_action is specified.
-        //
-        // Handle possible data accompanying notification message.
-        // [START handle_data_extras]
-        if (getIntent().getExtras() != null) {
-            for (String key : getIntent().getExtras().keySet()) {
+        if (getIntent().getExtras() != null)
+        {
+            for (String key : getIntent().getExtras().keySet())
+            {
                 Object value = getIntent().getExtras().get(key);
                 Log.e(TAG, "Key: " + key + " Value: " + value);
             }
         }
-        // [END handle_data_extras]
-
         Button subscribeButton = (Button) findViewById(R.id.subscribeButton);
         subscribeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // [START subscribe_topics]
                 FirebaseMessaging.getInstance().subscribeToTopic("news");
-                // [END subscribe_topics]
-
-                // Log and toast
                 String msg = "msg_subscribed";
                 Log.e(TAG, msg);
                 Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
@@ -59,34 +42,38 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Button logTokenButton = (Button) findViewById(R.id.logTokenButton);
-        logTokenButton.setOnClickListener(new View.OnClickListener() {
+        logTokenButton.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                // Get token
+            public void onClick(View v)
+            {
                 String token = FirebaseInstanceId.getInstance().getToken();
-
-                // Log and toast
                 String msg = "msg_token_fmt :" +token;
                 Log.e(TAG, msg);
                 Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
             }
         });
     }
-    private void checkPermissionsOfApp(String packageName) {
+    private void checkPermissionsOfApp(String packageName)
+    {
         PackageManager pm = getPackageManager();
-        try {
+        try
+        {
             final ApplicationInfo applicationInfo = pm.getApplicationInfo(packageName, PackageManager.GET_META_DATA);
             Log.d("AppLog", "Listing all permissions of app with PackageName: " + applicationInfo.packageName);
             PackageInfo packageInfo = pm.getPackageInfo(applicationInfo.packageName, PackageManager.GET_PERMISSIONS);
-            //Get Permissions
             String[] requestedPermissions = packageInfo.requestedPermissions;
-            if (requestedPermissions != null) {
-                for (String permission : requestedPermissions) {
+            if (requestedPermissions != null)
+            {
+                for (String permission : requestedPermissions)
+                {
                     boolean permissionGranted = pm.checkPermission(permission, packageName) == PackageManager.PERMISSION_GRANTED;
                     Log.d("AppLog", "permission:" + permission + " permissionGranted:" + permissionGranted);
                 }
             }
-        } catch (PackageManager.NameNotFoundException e) {
+        }
+        catch (PackageManager.NameNotFoundException e)
+        {
             e.printStackTrace();
         }
     }
